d587db36dd15e620313095889204d39d08a683e6
kafriyiegyamfi <kwabena.afriyiegyamfi@gmail.com>
2022-12-19 20:52:24 -0800

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a course requirement for alx-SE , this is an exercise based on pointers strings and arrays in C
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+void
+reset_to_98(int *n);
+void swapint(int *a, int*b);
+int _strlen(char *s);
+void _puts(char c);
+int _putchar(char c);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int r);
+char *_strenv(char *dest, char *src);
+void rev_string(char *s);
