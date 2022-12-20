6603bd5c5e69880989bc59664f741952f8d6db7b
kafriyiegyamfi <kwabena.afriyiegyamfi@gmail.com>
2022-12-19 22:06:38 -0800

    t-3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts- A function that prints a string, and add a new line
+ *
+ *@str: this is the input string
+ *
+ */
+
+void _puts(char *str)
+{
+int index;
+for (index = 0 ; str[index] = '\0'; index++)
+{
+_putchar(str[index]);
+}
+_putchar('\n');
+}
