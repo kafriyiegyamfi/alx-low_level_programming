051aaab8521747abb46e4276f6eb5f70369133e6
kafriyiegyamfi <kwabena.afriyiegyamfi@gmail.com>
2022-12-25 07:30:05 -0800

    t 5-7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  int n;
+for (n = 'z'; n >= 'a'; n--)
+{
+putchar(n);
+}
+
+putchar('\n');
+
+return (0);
+}
